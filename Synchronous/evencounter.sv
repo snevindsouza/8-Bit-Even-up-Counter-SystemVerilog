ffc176aa67bb0c41fe3d5686fac6de760165b0f4
Snevin Leoneel Dsouza <81520536+snevindsouza@users.noreply.github.com>
2023-10-14 04:26:19 +0530

    evencounter.sv

--- Synchronous/evencounter.sv
+++ Synchronous/evencounter.sv
@@ -2,7 +2,7 @@ module evencounter (input wire clk, reset, output [7:0]q);
     wire d2, d3, d4, d5, d6, d7, y3, y4, y5, y6, y7;
     dff D0 (q[0], clk, reset, q[0]);
     dff D1 (q[1], clk, reset, ~q[1]);
-    xorgate x2 (q[1], q[2], d2);
+    xorgate x2 (q[1], q[2], d2);       //you can aslo use the pre defined keywords instead
     dff D2 (q[2], clk, reset, d2);
     andgate a3 (q[1], q[2], y3);
     xorgate x3 (y3, q[3], d3);
